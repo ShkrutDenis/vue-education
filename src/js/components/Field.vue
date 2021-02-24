f0f1b52e189f43e34b3bb65f8ba38cd8f75aadcc
Denis Shkrut <Shkrut_denis@rambler.ru>
2021-02-24 21:54:06 +0200

    Remove realization

--- src/js/components/Field.vue
+++ src/js/components/Field.vue
@@ -1,41 +1,11 @@
 <template>
-  <div :class="{field:true, winner: isWinner, active: isActive}"
-       @click="fill">
-    {{ value }}
-  </div>
+  <!--TODO -->
 </template>
 
 <script>
 export default {
   name: 'Field',
-  props: {
-    id: {
-      type: Number,
-      require: true
-    },
-    isWinner: {
-      type: Boolean,
-      require: false,
-      default: false
-    },
-    value: {
-      type: String,
-      require: false,
-      default: ''
-    }
-  },
-  computed: {
-    isActive() {
-      return !this.value
-    }
-  },
-  methods: {
-    fill() {
-      if (this.isActive) {
-        this.$emit('fill', this.id)
-      }
-    }
-  }
+  // TODO
 }
 </script>
 
--- src/js/components/Game.vue
+++ src/js/components/Game.vue
@@ -2,19 +2,10 @@
   <div class="game">
     <div class="game-header">
       <div class="title">Крестики нолики</div>
-      <div v-if="!gameOver" class="subtitle">Ходит: {{ stepLabel }}</div>
-      <div v-else>
-        <div class="subtitle"> {{ winner ? 'Ура! Победитель "' + winner + '"' : 'Ничья' }}</div>
-      </div>
+      <!--TODO: player info block -->
     </div>
     <div class="game-field">
-      <field v-for="(f, i) in fields" :key="i"
-             :id="i"
-             :is-winner="f.isWinner"
-             :value="f.value"
-             @fill="changeValue"
-      ></field>
-      <div v-if="gameOver" class="game-button" @click="init">Играть снова</div>
+      <!--TODO: game field block -->
     </div>
   </div>
 </template>
@@ -25,95 +16,7 @@ const O = 'O'
 
 export default {
   name: 'Game',
-  data() {
-    return {
-      fields: [],
-      turn: X,
-      gameOver: false,
-      winner: null
-    }
-  },
-  created() {
-    this.init()
-  },
-  watch: {
-    turn(newVal, oldVal) {
-      if (this.hasWinner(oldVal)) {
-        this.winner = oldVal
-        this.gameOver = true
-        return
-      }
-      this.gameOver = this.isDraw()
-    }
-  },
-  computed: {
-    stepLabel() {
-      return this.turn === X ? 'Крестик' : 'Нолик'
-    }
-  },
-  methods: {
-    init() {
-      this.fields = []
-      for (let i = 0; i < 9; i++) {
-        this.fields.push({
-          isWinner: false,
-          value: null,
-        })
-      }
-      this.turn = X
-      this.gameOver = false
-      this.winner = null
-    },
-    changeValue(i) {
-      if (this.gameOver) return
-      this.fields[i].value = this.turn
-      this.turn === X ? this.turn = O : this.turn = X
-    },
-    isDraw() {
-      return !this.fields.some(f => {
-        if (!f.value) {
-          return true
-        }
-      })
-    },
-    hasWinner(winner) {
-      if (this.checkWinnerOnLines(winner)) {
-        return true
-      }
-      return this.checkWinnerOnDiagonals(winner)
-    },
-    checkWinnerOnLines(winner) {
-      for (let i = 0; i < 3; i++) {
-        let index = i
-        // check vertical line
-        if (this.fieldsIsSame(this.fields[index].value, this.fields[index + 3].value, this.fields[index + 6].value, winner)) {
-          this.fields[index].isWinner = this.fields[index + 3].isWinner = this.fields[index + 6].isWinner = true
-          return true
-        }
-        // check horizontal line
-        index = i + 2 * i
-        if (this.fieldsIsSame(this.fields[index].value, this.fields[index + 1].value, this.fields[index + 2].value, winner)) {
-          this.fields[index].isWinner = this.fields[index + 1].isWinner = this.fields[index + 2].isWinner = true
-          return true
-        }
-      }
-      return false
-    },
-    checkWinnerOnDiagonals(winner) {
-      if (this.fieldsIsSame(this.fields[0].value, this.fields[4].value, this.fields[8].value, winner)) {
-        this.fields[0].isWinner = this.fields[4].isWinner = this.fields[8].isWinner = true
-        return true
-      }
-      if (this.fieldsIsSame(this.fields[2].value, this.fields[4].value, this.fields[6].value, winner)) {
-        this.fields[2].isWinner = this.fields[4].isWinner = this.fields[6].isWinner = true
-        return true
-      }
-      return false
-    },
-    fieldsIsSame(f1, f2, f3, w) {
-      return f1 === f2 && f2 === f3 && f3 === w
-    }
-  }
+  // TODO
 }
 </script>
 
